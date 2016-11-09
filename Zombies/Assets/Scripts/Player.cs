3ac5e50cda18fbcc37645c74da85851c6e18bf40
Nicco Jacinto <nix_jacinto25@hotmail.com>
2016-11-09 16:49:09 -0500

    Stay a While and Chat
    Added a prototype chat system

---
+++ Zombies/Assets/Scripts/Player.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public string playerName;
+
+	void Start () {
+	
+	}
+	
+
+	void Update () {
+	
+	}
+}
